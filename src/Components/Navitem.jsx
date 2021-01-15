03d4819a6ce6066733d50d6113bee448f334dddb
MedAziz11 <58366672+MedAziz11@users.noreply.github.com>
2021-01-15 20:42:47 +0100

    navigation

--- src/Components/Navitem.jsx
+++ src/Components/Navitem.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Navitem = ({title, id}) => {
+const Navitem = ({title, id, cuurent}) => {
   return (
-    <div className="ml-6">
+    <div className="ml-6 pb-5">
       <a href="/test">
         <div className="flex items-center justify-end">
-          <div className="sm:w-10 border-b border-solid border-white w-5"></div>
+          <div className="sm:w-10 border-b-4 border-solid border-white w-5 "></div>
           <h1 className="sm:ml-3 sm:text-xs font-bold ml-1">{id}</h1>
         </div>
         <div className="text-right text-xs">{title}</div>
